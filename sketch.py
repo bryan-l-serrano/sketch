3ae5e5efcb4036b6d9505f0a2e9c169ff0e2d747
Bryan Serrano <bserran2@illinois.edu>
2016-06-16 14:09:22 -0500

    improved

--- sketch.py
+++ sketch.py
@@ -6,7 +6,7 @@ import sys, os
 if len(sys.argv) > 1:
 	image = sys.argv[1]
 else:
-	image = raw_input("enter thenameof the image file you would like to sketch")
+	image = raw_input("enter the name of the image file you would like to sketch")
 
 im = Image.open(image)
 width =  im.size[0]
@@ -15,6 +15,8 @@ picture = im.load()
 for y in range(1,height):
 	for x in range(1,width):
 		if abs(sum(picture[x,y]) - sum(picture[x-1,y-1])) >= 100:
+			im.putpixel((x-1,y-1), (0,0,0))
+		elif abs(sum(picture[x,y]) - sum(picture[x-1,y])) >= 100:
 			im.putpixel((x-1,y-1), (0,0,0))
 		else:
 			im.putpixel((x-1,y-1),(255,255,255))
